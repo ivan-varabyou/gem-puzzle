28fae2234ba589bc487507eafa6d5fb72ec5f531
Ivan Varabyou <onebelbiz@gmail.com>
2022-10-22 01:40:06 +0300

    feat: add drag function

--- gem-puzzle/src/js/game.mjs
+++ gem-puzzle/src/js/game.mjs
@@ -26,7 +26,7 @@ class Game {
     this.modal = document.querySelector('.modal');
 
     if (localStorage.getItem('gempuzzle')) {
-        let arg = JSON.parse(localStorage.getItem(`gempuzzle`))
+        let arg = this.loadStorage(`gempuzzle`)
         grid = arg[0];
         time = arg[1];
         moves = arg[2];
@@ -39,29 +39,32 @@ class Game {
     this.time = time;
     this.moves = moves;
     this.sound = 1;
+    this.lock = 0;
     this.pause = 0;
     this.empty = 0;
-    this.timeId = setInterval(() => {}, 1000);
+    this.timeId = {};
     this.status = 0;
     this.timerStatus = 0;
     this.win = win;
     this.soundFx = new Audio('../sound/fx.mp3');
-    this.cards = this.getCard(this.grid);
-    this.random = (random) || this.getRandomCard(this.cards);
+    this.setCard(this.grid);
+    this.random = (random) || this.getRandomCard();
     this.field = (field) || this.getField(this.random);
     this.event();
     this.frameSize.innerHTML = `<strong>Frame size:</strong> ${grid}x${grid}`;
   }
 
   isMove(e) {
-    const eX = Number(e.dataset.x);
-    const eY = Number(e.dataset.y);
-    const { empty } = this;
-    function moveLeft() { return !!((eY === empty.y && eX - 1 === empty.x)); }
-    function moveRight() { return !!((eY === empty.y && eX + 1 === empty.x)); }
-    function moveUp() { return !!((eY - 1 === empty.y && eX === empty.x)); }
-    function moveDown() { return !!((eY + 1 === empty.y && eX === empty.x)); }
-    if (moveDown() || moveUp() || moveRight() || moveLeft()) return true;
+    const eX = Number(e.target.dataset.x);
+    const eY = Number(e.target.dataset.y);
+    const nEmpty = document.querySelector('[data-id="0"]');
+    let y = Number(nEmpty.dataset.y);
+    let x = Number(nEmpty.dataset.x);
+    if ((eY === y && eX - 1 === x)) return 'up';
+    if ((eY === y && eX + 1 === x)) return 'down';
+    if ((eY - 1 === y && eX === x)) return 'left';
+    if ((eY + 1 === y && eX === x)) return 'right';
+    return false;
   }
 
   updateRandom() {
@@ -80,45 +83,57 @@ class Game {
     this.updateRandom();
     if (this.random.join('') === this.cards.join('')) {
         this.win = 1;
-        let score = this.moves/this.time;
         let minScore, topScore = [];
         this.addModal(`Hooray! You solved the puzzle in ${this.getMinut(this.time)} and ${this.moves} moves!`)
         if (localStorage.getItem('gempuzzleresult')) {
-          topScore = JSON.parse(localStorage.getItem('gempuzzleresult'));
+          topScore = this.loadStorage('gempuzzleresult');
           if(topScore.length >= 10) {
-            topScore.sort((a,b) => a[2] - b[2])
-            if(topScore[topScore.length-1][2] > score) {
+            topScore = sortResult(topScore)
+            if(topScore[topScore.length-1][1] > this.time) {
               topScore.pop()
-              topScore.push([this.moves,this.time,score])
+              topScore.push([this.moves,this.time])
             }
-            topScore.sort((a,b) => a[2] - b[2])
+            topScore = sortResult(topScore)
           } else {
-            topScore.sort((a,b) => a[2] - b[2])
-            topScore.push([this.moves,this.time,score])
+
+            topScore.push([this.moves,this.time])
+            topScore = sortResult(topScore)
           }
         } else {
-          topScore.push([this.moves,this.time,score])
+          topScore.push([this.moves,this.time])
         }
 
-        localStorage.setItem('gempuzzleresult', JSON.stringify(topScore))
+        this.saveStorage('gempuzzleresult', topScore)
+
+        function sortResult(array){
+          array.sort((a,b) => a[0] - b[0])
+          array.sort((a,b) => a[1] - b[1])
+          return  array
+        }
     }
   }
 
   moveCard(e) {
+    e.target.style.cssText = '';
     if (this.time >= 0) {
       this.startTimer(); this.status = 1;
     }
-    if (this.pause === 0 && this.time > 0) {
+
+    if (this.pause === 0 && this.time > 0 && this.lock === 0) {
       let x = Number(e.target.dataset.x);
       let y = Number(e.target.dataset.y);
       let id = Number(e.target.dataset.id);
-
-      if (this.isMove(e.target)) {
-       // if (nEmpty.className) nEmpty.className = `p-${x}-${y}`;
+      if (this.isMove(e)) {
+        let animation = this.isMove(e);
+        e.target.classList.add(animation)
+        this.lock = 1;
         let nEmpty = document.querySelector('[data-id="0"]');
         nEmpty.dataset.x = x;
         nEmpty.dataset.y = y;
         nEmpty.className = `p-${x}-${y}`;
+        setTimeout(() => {
+       // if (nEmpty.className) nEmpty.className = `p-${x}-${y}`;
+
         this.field[x][y].set(this.empty.x, this.empty.y);
         e.target.className = `p-${this.empty.x}-${this.empty.y}`;
         e.target.dataset.x = this.empty.x;
@@ -129,59 +144,64 @@ class Game {
         if (this.sound === 1) {
           this.soundFx.play();
         }
+        this.lock = 0;
         this.isWin();
+        e.target.classList.remove(animation)
+        }, 280);
+
+
       }
     }
   }
 
   event() {
+    this.dragCard();
     let x; let y; let id;
-
-    const pauseButton = document.getElementById('stop');
     const box = document.querySelector('.box');
     box.addEventListener('click', (e) => {
-    if(this.win === 0) {
-      if (e.target.dataset.id && e.target.dataset.id !== 0) {
-        this.moveCard(e)
-      }
-
-      if (e.target.id === 'save') {
-        this.timerStatus = 1;
-        this.saveStorage(
-          Number(this.grid),
-          Number(this.time),
-          Number(this.moves),
-          this.random,
-        );
-        this.timerStatus = 0;
-        this.addModal('Game saved successfully','<p>Reload the page to check if the data is safe. Сlick close to continue</p>');
-      }
-
-      if (e.target.id === 'stop') {
-        this.addModal('The game is paused','<p>Сlick close to continue</p>');
-      }
+      if(this.win === 0) {
 
 
-      if (e.target.id === 'sound') {
-        if (this.sound == 1) {
-          e.target.innerText = 'Sound OFF';
-          this.sound = 0;
-        } else {
-          e.target.innerText = 'Sound On';
-          this.sound = 1;
+        if (e.target.id === 'save') {
+          this.timerStatus = 1;
+          this.saveStorage(
+            'gempuzzle',
+            [
+              Number(this.grid),
+              Number(this.time),
+              Number(this.moves),
+              this.random
+            ]
+          );
+          this.timerStatus = 0;
+          this.addModal('Game saved successfully','<p>Reload the page to check if the data is safe. Сlick close to continue</p>');
         }
-      }
 
-      if (e.target.id == 'bodyfield') {
-        this.startTimer();
-      }
+        if (e.target.id === 'stop') {
+          this.addModal('The game is paused','<p>Сlick close to continue</p>');
+        }
 
-      if (e.target.dataset.game) {
-        this.startGame(e.target.dataset.game);
-        this.pause = 0;
-      }
 
-    }
+        if (e.target.id === 'sound') {
+          if (this.sound == 1) {
+            e.target.innerText = 'Sound OFF';
+            this.sound = 0;
+          } else {
+            e.target.innerText = 'Sound On';
+            this.sound = 1;
+          }
+        }
+
+        if (e.target.id == 'bodyfield') {
+          this.startTimer();
+        }
+
+        if (e.target.dataset.game) {
+          this.startGame(e.target.dataset.game);
+          this.pause = 0;
+        }
+
+      }
 
       if (e.target.id === 'start') {
         this.startGame();
@@ -190,17 +210,13 @@ class Game {
 
       if (e.target.id === 'result') {
         if(localStorage.getItem('gempuzzleresult')) {
-          let topScore = JSON.parse(localStorage.getItem('gempuzzleresult'));
-
+          let topScore = this.loadStorage('gempuzzleresult');
           this.addModal('The Top 10 Results',topScore);
         } else {
-
           this.addModal('The Top 10 Results','<p>No best results, be the first!</p>');
         }
       }
     });
-
-
 
     this.modal.addEventListener('click', (e) => {
       if(e.target.dataset.modal === 'close') {
@@ -209,24 +225,105 @@ class Game {
       }
     })
 
+
   }
 
-  getCard(grid = this.grid) {
+
+
+  dragCard() {
+      document.onmousedown = (e) => {
+        if(e.target.dataset && e.target.dataset.id && e.target.dataset.id !== 0) {
+        //if(isMouseWork(e) || !isMove(e)) return;
+        const coord = e.target.getBoundingClientRect()
+        const nEmpty = document.querySelector('[data-id="0"]');
+        const coordNEmpty = nEmpty.getBoundingClientRect()
+        let mousemove = 0;
+        e.target.style.cssText =
+        ` position: absolute;
+          width: ${coord.width}px;
+          height: ${coord.height}px;
+          top: ${coord.top}px;
+          left: ${coord.left}px;
+        `
+          let move = this.isMove(e);
+          document.onmousemove = (eM) => {
+            if(!move) return
+            mousemove = 1;
+
+            if(move == 'down' && (coord.top + coord.height + e.offsetY) >= eM.clientY) {
+              if(coord.top > eM.clientY || coord.left > eM.clientX || coord.left + coord.width < eM.clientX ) {
+                resetPosition(e)
+              }
+              e.target.style.top = `${eM.clientY - e.offsetY}px`;
+            }
+            if(move == 'up' && (coord.top - coord.height + e.offsetY) <= eM.clientY) {
+
+              if(coord.top + coord.height/1.5 < eM.clientY || coord.left > eM.clientX || coord.left + coord.width < eM.clientX ) {
+                resetPosition(e)
+              }
+              e.target.style.top = `${eM.clientY - e.offsetY}px`;
+            }
+
+
+            if(move == 'right' && (coord.left + coord.width + e.offsetX) >= eM.clientX) {
+              if(coord.left > eM.clientX || coord.top > eM.clientY || coord.top + coord.height < eM.clientY ) {
+                resetPosition(e)
+              }
+              e.target.style.left = `${eM.clientX - e.offsetX}px`;
+            }
+            if(move == 'left' && (coord.left - coord.width + e.offsetX) <= eM.clientX) {
+
+              if(coord.left + coord.width/1.5 < eM.clientX || coord.top > eM.clientY || coord.top + coord.height < eM.clientY ) {
+                resetPosition(e)
+              }
+              e.target.style.left = `${eM.clientX - e.offsetX}px`;
+            }
+          }
+          document.onmouseup = (eU) => {
+            if(mousemove === 0) {
+              this.moveCard(e)
+            } else {
+            if((move == 'up' && coord.top > eU.clientY - coord.height/2)
+            || (move == 'down' && coord.top + coord.height/2 < eU.clientY)
+            || (move == 'left' && coord.left > eU.clientX - coord.width/2)
+            || (move == 'right' && coord.left + coord.width/2 < eU.clientX)
+            ) {
+              this.moveCard(e);
+            }
+          }
+
+          resetPosition(e)
+          };
+          function resetPosition(e) {
+            e.target.style.cssText = '';
+            document.onmousemove = null;
+            document.onmouseup = null;
+          }
+        }
+      }
+
+  }
+
+
+
+
+
+
+  setCard(grid = this.grid) {
     const max = grid * grid;
     const cards = [];
     for (let i = 1; max > i; i++) {
       cards.push(i);
     }
     cards.push(0);
-
-    return cards;
+    this.cards = cards
   }
 
   getRandomCard(cards = this.cards) {
-    return cards.sort(() => Math.random() - Math.random());
+    return [...cards].sort(() => Math.random() - Math.random());
   }
 
-  getField(cards = this.cards) {
+  getField(cards = this.random) {
     const bodyField = document.getElementById('bodyfield');
     this.bodyField = bodyField;
     bodyField.style.cssText = `
@@ -269,7 +366,9 @@ class Game {
   setTimer(time = this.time) {
     if (this.timerStatus === 0) {
       this.timerStatus = 1;
-      this.time= 1
+      if(this.time === 0) {
+        this.time= 1
+      }
       clearInterval(this.timeId);
       this.timeId = setInterval(() => {
         if (this.pause === 0 && this.win === 0) {
@@ -291,14 +390,17 @@ class Game {
     this.timePanel.innerHTML = '<strong>Time:</strong> 00:00';
     this.movesPanel.innerHTML = '<strong>Moves:</strong> 0';
     this.frameSize.innerHTML = `<strong>Frame size:</strong> ${grid}x${grid}`;
-    this.cards = this.getCard(this.grid);
-    this.random = this.getRandomCard(this.cards);
-    this.field = this.getField(this.random);
+    this.cards = []; this.setCard(this.grid);
+    this.random = this.getRandomCard();
+    this.field = this.getField();
   }
 
-  saveStorage() {
-    const arg = [...arguments];
-    localStorage.setItem('gempuzzle', JSON.stringify(arg));
+  saveStorage(name, data) {
+    localStorage.setItem(name, JSON.stringify(data));
+  }
+
+  loadStorage(name, data) {
+    return JSON.parse(localStorage.getItem(name));
   }
 
   startTimer() {
@@ -332,10 +434,10 @@ class Game {
       bodyHtml = document.createElement('table');
       bodyHtml.className = 'table';
       let bodyHtmlTr;
-      bodyHtml.insertAdjacentHTML('beforeend',`<tr><td>Top<td><td>Mover<td><td>Time<td></tr>`)
+      bodyHtml.insertAdjacentHTML('beforeend',`<tr><td>Top</td><td>Mover</td><td>Time</td></tr>`)
       body.forEach((v,i) => {
         bodyHtmlTr = `
-          <tr><td>${i+1}<td><td>${v[0]}<td><td>${this.getMinut(v[0])}</tr>
+          <tr><td>${i+1}</td><td>${v[0]}</td><td>${this.getMinut(v[1])}</td></tr>
         `
         bodyHtml.insertAdjacentHTML('beforeend',bodyHtmlTr)
       })
